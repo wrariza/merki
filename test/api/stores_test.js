d3ecb764253dcc352fac5e3b20b73c2aec3e6598
Wiliam Ricardo Ariza Vèlez <wrariza@Wiliams-MacBook-Pro.local>
2017-02-19 14:33:31 -0500

    agregando soporte para coveralls y nyc

---
+++ test/api/stores_test.js
@@ -0,0 +1,13 @@
+import test from 'ava'
+
+test('foo will run', (t) => {
+  t.pass()
+})
+
+test('moo will also run', (t) => {
+  t.pass()
+})
+
+test.only('boo will run but not exclusively', (t) => {
+  t.pass()
+})
