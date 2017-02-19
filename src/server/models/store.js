144748ea3a1006b955cd7eb191fa9f361306c576
Wiliam Ricardo Ariza Vèlez <wrariza@Wiliams-MacBook-Pro.local>
2017-02-19 14:58:39 -0500

    refactorizando carpetas para api y modelos

---
+++ src/server/models/store.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const StoreSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  location: {
+    type: [Number],  // [<longitude>, <latitude>]
+    index: '2d',    // create the geospatial index
+  },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+  description: String,
+})
+
+export default mongoose.model('Store', StoreSchema)
