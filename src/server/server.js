f0a471c9a7bf4ccad8b019bf221b60204be6f3cd
william ariza <skchard09@gmail.com>
2017-03-12 23:12:40 -0500

    config heroku webpack

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ mongoose.connect(connectMongo)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 
 app.use('/api', api)
 
