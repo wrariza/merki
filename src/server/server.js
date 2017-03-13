eee46b730a0377f48d9da9d42ccdc39777520287
Wiliam Ricardo Ariza Vèlez <wrariza@Wiliams-MacBook-Pro.local>
2017-03-12 22:05:57 -0500

    configuracion heroku

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,16 @@ import bodyParser from 'body-parser'
 import api from './api'
 
 const app = express()
-mongoose.connect('mongodb://localhost/apptienda')
+const port = process.env.PORT || 5000;
+const connectMongo = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/apptienda'
+
+mongoose.connect(connectMongo, (err, res) => {
+  if (err) {
+    console.log ('ERROR connecting to: ' + connectMongo + '. ' + err)
+  } else {
+    console.log ('Succeeded connected to: ' + connectMongo)
+  }
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -14,6 +23,6 @@ app.use(express.static('public'))
 
 app.use('/api', api)
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Start server in listen port 3000')
 })
